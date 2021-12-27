41bc09aba92e37c520d49fca951ed410c8bd2f44
sisterofDC <yinxinwangfunny@qq.com>
2021-12-27 16:34:09 +0800

    basic

--- app/src/main/java/com/example/handwritetwo/MainActivity.kt
+++ app/src/main/java/com/example/handwritetwo/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.handwritetwo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.tensorflow.lite.Interpreter
+import java.io.FileInputStream
+import java.io.IOException
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+
+const val TAG = "sisterofdc"
 
 class MainActivity : AppCompatActivity() {
+    lateinit var interpreter: Interpreter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try {
+            interpreter = Interpreter(loadModelFile(),null)
+        }catch (e: IOException){
+            Log.d(TAG,"$e")
+        }
+
     }
+
+
+    private fun loadModelFile(): MappedByteBuffer {
+        val assetsProvider = assets
+        val assetFileDescriptor = assetsProvider.openFd("")
+        val fileInputStream: FileInputStream = FileInputStream(assetFileDescriptor.fileDescriptor)
+        val fileChannel: FileChannel = fileInputStream.channel
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,assetFileDescriptor.startOffset,assetFileDescriptor.length)
+    }
+
 }
